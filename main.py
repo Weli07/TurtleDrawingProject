c8701236010ea8f7b4907bf413f8bef42fec0189
Veli ÇIRAGÖZ <veli_ciragoz@hotmail.com>
2025-11-28 10:42:58 +0300

    Proje oluşturuldu

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+
+drawing_board=turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Turtle Drawing")
+
+turtle_instance=turtle.Turtle()
+turtle_instance.forward(100)
+turtle.done()
