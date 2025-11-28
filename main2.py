d450165106a7e122b53139e435650a017f335fc7
Veli ÇIRAGÖZ <veli_ciragoz@hotmail.com>
2025-11-28 15:24:42 +0300

    2 farklı kare çizme eklendi.

--- main2.py
+++ main2.py
@@ -1,8 +1,14 @@
+
 import turtle
+
 
 drawing_board=turtle.Screen()
 drawing_board.bgcolor("green")
 drawing_board.title("Turtle Drawing Screen")
+
+
+
+
 
 turtle_instance=turtle.Turtle()
 turtle_instance.forward(100)
@@ -10,13 +16,13 @@ turtle_instance.forward(100)
 turtle_instance.left(90)
 turtle_instance.forward(100)
 
-turtle_instance.left(270)
-turtle_instance.backward(100)
 
-turtle_instance.left(270)
-turtle_instance.forward(100)
+turtle_instance_2=turtle.Turtle()
+turtle_instance_2.left(90)
+turtle_instance_2.forward(100)
 
-
+turtle_instance_2.left(270)
+turtle_instance_2.forward(100)
 
 
 turtle.done()
