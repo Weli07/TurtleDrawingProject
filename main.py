f95534f74b9a09f537ae5203a230c5a3f754c3b2
Veli ÇIRAGÖZ <veli_ciragoz@hotmail.com>
2025-11-28 15:51:34 +0300

    yıldız ve polygon eklendi.

--- main.py
+++ main.py
@@ -5,18 +5,62 @@ drawing_board.bgcolor("green")
 drawing_board.title("Turtle Drawing Screen")
 
 turtle_instance=turtle.Turtle()
+
+'''
+#Kare çizmek 
 turtle_instance.forward(100)
 
 turtle_instance.left(90)
 turtle_instance.forward(100)
 
-turtle_instance.left(270)
-turtle_instance.backward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
 
-turtle_instance.left(270)
+turtle_instance.left(90)
 turtle_instance.forward(100)
 
 
+for i in range(4):
+    turtle_instance.left(90)
+    turtle_instance.forward(100)
 
+syc=1
+while syc<5:
+    turtle_instance.left(90)
+    turtle_instance.forward(100)
+    syc=syc+1
+    
+
+
+# yıldız çizmek
+turtle_instance.left(30)
+turtle_instance.forward(200)
+turtle_instance.left(210)
+turtle_instance.forward(200)
+
+turtle_instance.left(210)
+turtle_instance.forward(200)
+
+turtle_instance.left(210)
+turtle_instance.forward(200)
+
+turtle_instance.left(225)
+turtle_instance.forward(200)
+
+for i in range(5):
+    turtle_instance.left(144)
+    turtle_instance.forward(200)
+    
+'''
+
+# polygon
+
+num_slides=5
+angle=360.0/num_slides*2
+side_length=200
+
+for i in range(num_slides):
+    turtle_instance.left(angle)
+    turtle_instance.forward(side_length)
 
 turtle.done()
